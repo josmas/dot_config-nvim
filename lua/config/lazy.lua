fcf32ac83da4b713ca59e8233f2c37c74709e1c6
josmas <josmasflores@gmail.com>
2024-05-11 17:12:46 +0100

    add plugins for docker, markdown and yaml

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,6 +16,9 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.lang.yaml" },
     -- import/override with your plugins
     { import = "plugins" },
   },
