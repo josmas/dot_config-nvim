9dec4777d53c95274509210127ec087583e6dc54
josmas <josmasflores@gmail.com>
2024-05-11 14:17:30 +0100

    Add modifiers to neo-tree to open files with l

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,87 @@
+return {
+  -- Change default permissions for files created via Neo-tree
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+        window = {
+          mappings = {
+            ["L"] = "open_nofocus",
+          },
+        },
+        commands = {
+          open_nofocus = function(state)
+            require("neo-tree.sources.filesystem.commands").open(state)
+            vim.schedule(function()
+              vim.cmd([[Neotree focus]])
+            end)
+          end,
+        },
+      },
+      window = {
+        mappings = {
+          ["l"] = "open",
+          ["h"] = "close_node",
+        },
+      },
+      event_handlers = {
+        --[[ change default permissions of newly created files ]]
+        {
+          event = "file_added",
+          handler = function(destination)
+            local uv = vim.loop
+            local file_info = uv.fs_stat(destination)
+            if not file_info then
+              return
+            elseif file_info.type == "file" then
+              uv.fs_chmod(destination, 436) -- (436 base 10) == (664 base 8)
+            elseif file_info.type == "directory" then
+              uv.fs_chmod(destination, 509) -- 644 does not work for directories I guess?
+            end
+          end,
+        },
+      },
+    },
+  },
+  -- Modify `flash.nvim`
+  {
+    "folke/flash.nvim",
+    keys = {
+      -- Disable default mappings, cuz they conflict with `vim-surround`
+      { "s", mode = { "n", "x", "o" }, false },
+      { "S", mode = { "n", "x", "o" }, false },
+      {
+        "m",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash").jump()
+        end,
+        desc = "Flash",
+      },
+      {
+        "M",
+        mode = { "n", "o", "x" },
+        function()
+          require("flash").treesitter()
+        end,
+        desc = "Flash Treesitter",
+      },
+    },
+    opts = {
+      modes = {
+        -- Disable labels for regular search with `/`
+        search = {
+          enabled = false,
+        },
+        -- Modify options used by `flash` when doing `f`, `F`, `t`, `T` motions
+        char = {
+          jump_labels = true,
+        },
+      },
+    },
+  },
+}
