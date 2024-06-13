d7859a9f4ee312c56e2dc23413855d6ec9942cb2
josmas <josmasflores@gmail.com>
2024-06-13 13:10:37 +0100

    add prettier and eslint as extras

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,6 +18,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     -- import/override with your plugins
     { import = "plugins" },
